b976055a9085fa9134ac2b4c7ee3b6b13d4cef12
jveiga17 <jveiga17@icloud.com>
2024-03-26 18:17:28 +0200

    changes 3

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -1,4 +1,5 @@
 
+
 import axios from "axios";
 
 const upload = async (file) => {
@@ -7,26 +8,12 @@ const upload = async (file) => {
   data.append("upload_preset", "fiverr");
 
   try {
-    console.log('API Key:', process.env.REACT_APP_CLOUDINARY_API_KEY);
-
-
-    const res = await axios.post(
-      "https://api.cloudinary.com/v1_1/joaoveiga/image/upload",
-      data,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "X-Requested-With": "XMLHttpRequest",
-          "X-Api-Key": process.env.REACT_APP_CLOUDINARY_API_KEY, // Access API key from .env
-        },
-      }
-    );
+    const res = await axios.post("https://api.cloudinary.com/v1_1/joaoveiga/image/upload", data);
 
     const { url } = res.data;
     return url;
   } catch (err) {
-    console.log("Problem in the upload:", err);
-    throw err; // Rethrow the error to handle it elsewhere if needed
+    console.log(err);
   }
 };
 
