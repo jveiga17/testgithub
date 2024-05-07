d06c78ec7b4b5b6c8834885e60204f2400e2a6e8
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-05-07 17:14:01 +0300

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 // Use the REACT_APP_ prefix for custom environment variables in a React app
+/*
 const baseURL = process.env.REACT_APP_NODE_ENV === 'production'
     ? 'https://testgithub-7p32.onrender.com/api'
     : 'http://localhost:8800/api';
-
+    */
+const baseURL = 'https://testgithub-7p32.onrender.com/api'
 const newRequest = axios.create({
   baseURL: baseURL,
   withCredentials: true,
