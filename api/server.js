ce5a47d86923d7fa06c88c7f846af12d996a1fe6
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-05-07 14:34:29 +0300

    Update server.js - 4 cors

--- api/server.js
+++ api/server.js
@@ -25,8 +25,10 @@ const connect = async () => {
 };
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://testgithub-7p32.onrender.com"],
-    credentials: true
+    origin: ['http://localhost:5173', 'https://testgithub-7p32.onrender.com'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
