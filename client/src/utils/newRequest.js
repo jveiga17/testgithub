cc2afaf96354552a5b4b7ae69204463a7b063a0f
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-05-07 14:25:34 +0300

    Update newRequest.js - 3 node env

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,13 @@
-const baseURL = process.env.NODE_ENV === 'production' 
-    ? 'https://testgithub-7p32.onrender.com/api' 
+import axios from "axios";
+
+// Use the REACT_APP_ prefix for custom environment variables in a React app
+const baseURL = process.env.REACT_APP_NODE_ENV === 'production'
+    ? 'https://testgithub-7p32.onrender.com/api'
     : 'http://localhost:8800/api';
 
 const newRequest = axios.create({
-    baseURL: baseURL,
-    withCredentials: true,
+  baseURL: baseURL,
+  withCredentials: true,
 });
+
+export default newRequest;
