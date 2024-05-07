a3d25b6c0f5134d45fb335ca83bad572027711d0
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-05-07 14:15:24 +0300

    Update newRequest.js - node_env 2

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+// Determine the baseURL based on the environment
+const baseURL = process.env.NODE_ENV === 'production' 
+    ? 'https://testgithub-7p32.onrender.com/api' 
+    : 'http://localhost:8800/api';
+
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: baseURL,
   withCredentials: true,
 });
 
