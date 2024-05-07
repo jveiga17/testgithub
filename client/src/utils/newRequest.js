743f6e8b23e2e1a636e451e875c256c5f67e2803
Joao Veiga <144750954+jveiga17@users.noreply.github.com>
2024-05-07 14:01:04 +0300

    Update newRequest.js - node_env

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+const baseURL = process.env.NODE_ENV === 'production' 
+    ? 'https://testgithub-7p32.onrender.com/api' 
+    : 'http://localhost:8800/api';
 
 const newRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8800/api", // Add fallback for local development
-  withCredentials: true,
+    baseURL: baseURL,
+    withCredentials: true,
 });
-
-export default newRequest;
